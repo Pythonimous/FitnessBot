8554c93f80643a93dc04370f0a06d21af8e6588c
Anna Safaryan <Anna.Safaryan-813@yandex.ru>
2018-11-28 12:36:55 +0300

    Тело бота

--- telegram/bot.py
+++ telegram/bot.py
@@ -1,58 +1,54 @@
+import random
+import db_sqlite
 from telegram.ext import Filters
 from telegram.ext import MessageHandler, CommandHandler, CallbackQueryHandler
 from telegram.ext import Updater
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram.message import Message
-import drive
 
 token='764336853:AAHHE0kNv9m95kUnCt5xmsX2w2av9LwTnaw'
 
 class Bot():
 
     def __init__(self, token):
-        self.users = {}
+        self.chats = {} # chat_id: user_id
+        self.users = {} # user_id: chat_id
 
         self.updater = Updater(token=token)  # заводим апдейтер
         self.updater.dispatcher.add_handler(CommandHandler('start', self.start))
-        self.updater.dispatcher.add_handler(CommandHandler('name', self.name))
+        self.updater.dispatcher.add_handler(CommandHandler('history', self.history))
         self.updater.dispatcher.add_handler(MessageHandler(Filters.text, self.handle_message))  # обработчик сообщений
         self.updater.dispatcher.add_handler((CallbackQueryHandler(self.button)))
+
 
     menu_markup = [[KeyboardButton('Расписание'), KeyboardButton('Список')],
                     [KeyboardButton('Записаться'), KeyboardButton('Отписаться')]]
     menu = ReplyKeyboardMarkup(menu_markup, resize_keyboard=True, one_time_keyboard=True)
 
-
     timetable_markup = [[InlineKeyboardButton('Занятие 1', callback_data='1'),
                      InlineKeyboardButton('Занятие 2', callback_data='2')]]
     timetable = InlineKeyboardMarkup(timetable_markup)
 
-    timetable_html = '<b>Расписание:</b>\nЗанятие 1\nЗанятие 2'
+    timetable_html = '<h1 align="center">Расписание</h1><ul> {} </ul>'
+    lesson_html = '<li>{}, {} {}. Осталось мест: {}'
 
 
     def start(self, bot, update):
-        bot.sendMessage(chat_id=update.message.chat_id, text='Здравствуйте! Что бы вы хотели сделать?', reply_markup=self.menu)
-
-    def name(self, bot, update):
-        print('Received', update.message)
-        id, text, mess = self.spec_mess(bot, update)
-        print(id, '\n')
-        print(text, '\n')
-        print(mess)
-        bot.sendMessage(chat_id=id, text='text: {}\n message: {}'.format(text, mess))
-
-        '''
-        bot.sendMessage(chat_id=update.message.chat_id, text='Представьтесь, пожалуйста:')
-        #print('text', update.message.text)
-        print('reply', Message.reply_to_message)
-        print('forward from', Message.forward_from)
-        print('text', Message.text)
-        print('rep text', Message.reply_text((update.message)))
-        #self.users[update.message.chat_id] = update.message.username
-        #print()
-        #print('Users:', self.users)
-        '''
+        if update.message.chat_id in self.chats: # чат не новый
+            print('users\t', self.users)
+            print('chats\t', self.chats)
+            bot.sendMessage(chat_id=update.message.chat_id, text='Здравствуйте, {}! Что бы вы хотели сделать?'.format(self.chats.get(update.message.chat_id)), reply_markup=self.menu)
+        else:
+            update.message.reply_text('Здравствуйте! Похоже, Вы у нас впервые. Я присвою Вам id -- пожалуйста, запомните его, чтобы мой брат в ВК мог узнать Вас.')
+            #user_ids = (id[0] for id in db_sqlite.list_all()) # кто есть в базе
+            #print('users in base\t', user_ids)
+            user_id = random.randint(1000, 9999) # четырёхзначное число
+            #while user_id in user_ids:
+            #    user_id = random.randint(1000, 9999)
+            self.chats[update.message.chat_id] = user_id
+            self.users[user_id] = update.message.chat_id
+            update.message.reply_text('{}'.format(user_id))
 
 
     def button(self, bot, update):
@@ -67,7 +63,13 @@ class Bot():
         print(update)
 
         if update.message.text.lower() == 'расписание':
-            bot.sendMessage(chat_id=update.message.chat_id, parse_mode='HTML', text=self.timetable_html)
+            #bot.sendMessage(chat_id=update.message.chat_id, text='HTML')
+            timetable = db_sqlite.list_timetable()
+            lessons = ''
+            for l in timetable: # добавляем строчки с уроками
+                lessons = lessons+self.lesson_html.format(l[0], l[1], l[2], l[3])
+            bot.sendMessage(chat_id=update.message.chat_id, parse_mode='HTML', text=self.timetable_html.format(lessons))
+
 
         elif update.message.text.lower() == 'записаться':
             bot.sendMessage(chat_id=update.message.chat_id, text='Выберите занятие:', reply_markup=self.timetable)
@@ -78,10 +80,11 @@ class Bot():
         elif update.message.text.lower() == 'список':
             bot.sendMessage(chat_id=update.message.chat_id, text='Выберите занятие:', reply_markup=self.timetable)
 
-        elif update.message.text.lower() == 'файл':
-            open('file.txt', 'w').write('Hello!')
-            drive.to_drive('file.txt')
-            bot.sendMessage(chat_id=update.message.chat_id, text='Success!')
+        elif update.message.text.lower() == 'sql':
+            bot.sendMessage(chat_id=update.message.chat_id, text='Подключаюсь к sql...')
+            data = db_sqlite.list_all()
+            print(data)
+            bot.sendMessage(chat_id=update.message.chat_id, text='{}'.format(data))
 
 
         else:
@@ -92,8 +95,12 @@ class Bot():
         message = input('Сообщение: ')
         chat_id=update.message.chat_id
         text=update.message.text
-        return chat_id, text, chat_id
+        return chat_id, text, message
 
+    def history(bot):
+        print('history')
+        for update in bot.get_updates():
+            print(update)
 
 
 if __name__ == "__main__":
