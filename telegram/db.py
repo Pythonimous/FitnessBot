06dfa7612c0b3d1bd19943ef757f0014b15b3113
Anna Safaryan <Anna.Safaryan-813@yandex.ru>
2018-12-12 01:39:10 +0300

    Модуль для sqlite #5

--- telegram/db.py
+++ telegram/db.py
@@ -2,35 +2,28 @@
 import random
 import sqlite3
 
-'''
-# Очистка базы
-conn = sqlite3.connect('fitness.sqlite')
-c = conn.cursor()
-lesson_ids = c.execute('SELECT lesson_id FROM timetable').fetchall() # список старых занятий
-c.execute("DROP TABLE IF EXISTS users")
-c.execute("DROP TABLE IF EXISTS timetable")
-for lesson_id in lesson_ids:
-    c.execute("DROP TABLE IF EXISTS z" + str(lesson_id)) # удаляем таблицы с занятиями
-
-conn.commit() #отправка данных в базу
-c.close()
-conn.close()
-'''
 
 # РАБОТА С ПОЛЬЗОВАТЕЛЯМИ
 #Создание таблицы
 conn = sqlite3.connect('fitness.sqlite')
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS users (user_id int primary key, surname varchar, name varchar, gruppa varchar)")
+c.execute("CREATE TABLE IF NOT EXISTS users (user_id int primary key, surname varchar, name varchar, gruppa varchar, status varchar)")
 conn.commit() #отправка данных в базу
 c.close()
 conn.close()
 
-#добавить пользователя в базу
-def add_user(id, surname, name, gruppa):
+#добавить студента в базу
+def add_student(id, surname, name, gruppa):
     conn = sqlite3.connect('fitness.sqlite')
     c = conn.cursor()
-    c.execute("INSERT INTO users (user_id, surname, name, gruppa) VALUES (?, ?, ?, ?)",(id, surname, name, gruppa))
+    c.execute("INSERT INTO users (user_id, surname, name, gruppa, status) VALUES (?, ?, ?, ?, ?)",(id, surname, name, gruppa, 'student'))
+    conn.commit()
+
+#добавить препода в базу
+def add_teacher(id, surname, name):
+    conn = sqlite3.connect('fitness.sqlite')
+    c = conn.cursor()
+    c.execute("INSERT INTO users (user_id, surname, name, gruppa, status) VALUES (?, ?, ?, ?, ?)",(id, surname, name, None, 'teacher'))
     conn.commit()
 
     # закрываем соединение с базой
@@ -62,7 +55,6 @@ def list_them(ids):
         c.execute('SELECT surname, name, gruppa FROM users WHERE user_id={}'.format(id))
         user = c.fetchone()
         users.append(user)
-
     # закрываем соединение с базой
     c.close()
     conn.close()
@@ -70,17 +62,29 @@ def list_them(ids):
     return users
 
 # изменить данные в нужном столбце
-def update_name(surname, name, id):
+def change_name(surname, name, gruppa, id):
     conn = sqlite3.connect('fitness.sqlite')
     c = conn.cursor()
 
     c.execute('UPDATE users SET surname=? WHERE user_id=?',(surname, id))
     c.execute('UPDATE users SET name=? WHERE user_id=?',(name, id))
+    c.execute('UPDATE users SET gruppa=? WHERE user_id=?',(gruppa, id))
     conn.commit()
 
     # закрываем соединение с базой
     c.close()
     conn.close()
+
+
+def get_status(id):
+    conn = sqlite3.connect('fitness.sqlite')
+    c = conn.cursor()
+
+    status = c.execute('SELECT status FROM users WHERE user_id={}'.format(id)).fetchone()[0]
+
+    c.close()
+    conn.close()
+    return status
 
 
 def del_user(id):
@@ -104,7 +108,7 @@ conn.commit() #отправка данных в базу
 c.close()
 conn.close()
 
-# список всех занятий с оставшимися местами
+# список всех занятий
 def list_timetable():
     conn = sqlite3.connect('fitness.sqlite')
     c = conn.cursor()
@@ -117,6 +121,23 @@ def list_timetable():
     conn.close()
 
     return lessons
+
+
+# список всех занятий с оставшимися местами
+def list_open():
+    conn = sqlite3.connect('fitness.sqlite')
+    c = conn.cursor()
+
+    c.execute('SELECT lesson, day, time, left, lesson_id FROM timetable WHERE left>0')
+    lessons = c.fetchall()
+
+    # закрываем соединение с базой
+    c.close()
+    conn.close()
+
+    return lessons
+
+
 
 # создать занятие
 def add_lesson(lesson, day, time, g_size):
@@ -173,8 +194,6 @@ def add_to_lesson (lesson_id, user_id):
         c.close()
         conn.close()
         raise Exception('Мест больше нет!')
-
-
 
 
 # id всех записавшихся
@@ -238,7 +257,7 @@ def user_lessons(user_id):
 def get_lesson(lesson_id):
     conn = sqlite3.connect('fitness.sqlite')
     c = conn.cursor()
-    lesson = c.execute('SELECT lesson, day, time, g_size, left FROM timetable WHERE lesson_id={}'.format(lesson_id)).fetchone()
+    lesson = c.execute('SELECT lesson, day, time, g_size, left, lesson_id FROM timetable WHERE lesson_id={}'.format(lesson_id)).fetchone()
     c.close()
     conn.close()
     return lesson
@@ -259,3 +278,20 @@ def del_lesson(lesson_id):
     conn.close()
 
     return user_ids
+
+
+# Очистка расписания
+def reset_timetable():
+    conn = sqlite3.connect('fitness.sqlite')
+    c = conn.cursor()
+
+    lesson_ids = c.execute('SELECT lesson_id FROM timetable').fetchall() # список старых занятий
+    #print(lesson_ids)
+    for lesson in lesson_ids:
+        lesson_id = lesson[0]
+        #print(lesson_id)
+        c.execute('DROP TABLE z' + str(lesson_id)) # удаляем занятие
+    c.execute('DELETE FROM timetable')
+    conn.commit() #отправка данных в базу
+    c.close()
+    conn.close()
